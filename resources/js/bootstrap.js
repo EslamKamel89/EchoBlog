47504b150cedf52fc4e04fd04a322b57d4ad5560
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-09 21:58:10 +0200

    Add Clear command for clearing Laravel data and integrate Pusher for real-time event handling

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
+import Echo from "laravel-echo";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+window.Echo = new Echo({
+    broadcaster: "pusher",
+    key: "d81ed4f41e49070cb6f4",
+    cluster: "eu",
+    forceTLS: true,
+});
+
+var channel = Echo.channel("my-channel");
+channel.listen(".my-event", function (data) {
+    alert(JSON.stringify(data));
+});
