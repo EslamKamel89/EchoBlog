44ccf7ec7350a8426ea509b21eb5cdb0c0ce9595
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-09 22:09:37 +0200

    Refactor Pusher integration in bootstrap.js and enable logging for debugging

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,17 +1,27 @@
 import axios from "axios";
-import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-window.Echo = new Echo({
-    broadcaster: "pusher",
-    key: "d81ed4f41e49070cb6f4",
+// window.Echo = new Echo({
+//     broadcaster: "pusher",
+//     key: "d81ed4f41e49070cb6f4",
+//     cluster: "eu",
+//     forceTLS: true,
+// });
+
+// var channel = Echo.channel("my-channel");
+// channel.listen("my-event", function (data) {
+//     alert(JSON.stringify(data));
+// });
+Pusher.logToConsole = true;
+
+var pusher = new Pusher("d81ed4f41e49070cb6f4", {
     cluster: "eu",
-    forceTLS: true,
 });
 
-var channel = Echo.channel("my-channel");
-channel.listen(".my-event", function (data) {
+var channel = pusher.subscribe("my-channel");
+channel.bind("my-event", function (data) {
     alert(JSON.stringify(data));
 });
